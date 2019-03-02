0cfbfa004bfd187bd801d94829bca38b280c982a
svaskeli <svaskeli@student.42.fr>
2019-03-02 10:47:29 +0100

    fixed small mistakes

--- 01_dockerfiles/ex03/start_VM.sh
+++ 01_dockerfiles/ex03/start_VM.sh
@@ -2,5 +2,5 @@ docker-machine stop Char
 VBoxManage modifyvm Char --cpus 2
 VBoxManage modifyvm Char --memory 4096
 docker-machine start Char
-sleep 2
-eval $(docker-machine env Char)
+
+# eval $(docker-machine env Char)
